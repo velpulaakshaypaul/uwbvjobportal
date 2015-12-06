ab040b97dd8f8b789823232f148f2f6156656b1a
jacobmodayil <jacobmodayil@outlook.com>
2015-12-06 03:44:57 -0600

    Delete 20151206045805_add_paidemployment_application_to_education.rb

--- db/migrate/20151206045805_add_paidemployment_application_to_education.rb
+++
@@ -1,5 +0,0 @@
-class AddPaidemploymentApplicationToEducation < ActiveRecord::Migration
-  def change
-    add_reference :educations, :PaidemploymentApplication, index: true, foreign_key: true
-  end
-end
